a1fb53483d602a1dc11880b3a605e31d70ce92c7
nitika02 <36486523+nitika02@users.noreply.github.com>
2022-08-15 02:04:41 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+const characterData = {
+    hero: {
+        name: "Wizard",
+        avatar: "images/wizard.webp",
+        health: 60,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    orc: {
+        name: "Orc",
+        avatar: "images/orc.jpg",
+        health: 30,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "images/demon.jpg",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "images/goblin.webp",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
+    }
+}
+
+export default characterData
